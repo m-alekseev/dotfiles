9842ee297a51651703a7bd63dba5807d2a6506ef
Veeskelly <mkl-alekseev@yandex.ru>
2025-06-18 00:18:11 +0300

    supertab redo

--- nvim/lua/plugins/plugs.lua
+++ nvim/lua/plugins/plugs.lua
@@ -25,8 +25,24 @@ return {
 		opts = {
 			keymap = {
 				-- preset = "super-tab",
-				["<C-j>"] = { "select_next" },
-				["<C-k>"] = { "select_prev" },
+				["C-space"] = { "show_documentation", "hide_documentation" },
+				["<C-e>"] = { "hide", "fallback" },
+				["<Tab>"] = {
+					function(cmp)
+						if cmp.snippet_active() then
+							return cmp.accept()
+						else
+							return cmp.select_and_accept()
+						end
+					end,
+					"snippet_forward",
+					"fallback",
+				},
+				["<S-Tab>"] = { "snippet_backward", "fallback" },
+				["<C-j>"] = { "select_next", "fallback" },
+				["<C-k>"] = { "select_prev", "fallback" },
+				["<C-u>"] = { "scroll_documentation_up", "fallback" },
+				["<C-d>"] = { "scroll_documentation_down", "fallback" },
 			},
 		},
 	},
