42d5e78a45c85c4fa6351413fb934b5fdfe517ed
Mikhail Alekseev <alekseevma@sibur.ru>
2025-06-17 17:27:38 +0300

    upd

--- nvim/lua/plugins/plugs.lua
+++ nvim/lua/plugins/plugs.lua
@@ -1,15 +1,16 @@
 return {
 	{
+		-- need it to python's venv to work, lol
 		"nvim-telescope/telescope.nvim",
-		keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-		},
+		-- keys = {
+		--     -- add a keymap to browse plugin files
+		--     -- stylua: ignore
+		--     {
+		--       "<leader>fp",
+		--       function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+		--       desc = "Find Plugin File",
+		--     },
+		-- },
 	},
 	{
 		"folke/which-key.nvim",
@@ -21,16 +22,10 @@ return {
 		"saghen/blink.cmp",
 		opts = {
 			keymap = {
-				preset = "super-tab",
+				-- preset = "super-tab",
 				["<C-j>"] = { "select_next" },
 				["<C-k>"] = { "select_prev" },
 			},
-		},
-	},
-	{
-		"catgoose/nvim-colorizer.lua",
-		event = "BufReadPre",
-		opts = { -- set to setup table
 		},
 	},
 	{
