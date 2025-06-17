da5d8eb83f2b20c166ba02ce0a4eccdecbb54f0b
Mikhail Alekseev <alekseevma@sibur.ru>
2025-06-17 14:07:35 +0300

    upd

--- nvim/lua/plugins/plugs.lua
+++ nvim/lua/plugins/plugs.lua
@@ -1,7 +1,7 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
       -- add a keymap to browse plugin files
       -- stylua: ignore
       {
@@ -9,36 +9,36 @@ return {
         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
         desc = "Find Plugin File",
       },
-    },
-  },
-  {
-    "folke/which-key.nvim",
-    opts = {
-      preset = "modern",
-    },
-  },
-  {
-    "saghen/blink.cmp",
-    opts = {
-      keymap = {
-        preset = "super-tab",
-        ["<C-j>"] = { "select_next" },
-        ["<C-k>"] = { "select_prev" },
-      },
-    },
-  },
-  {
-    "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = { -- set to setup table
-    },
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      sections = {
-        lualine_z = {},
-      },
-    },
-  },
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		opts = {
+			preset = "modern",
+		},
+	},
+	{
+		"saghen/blink.cmp",
+		opts = {
+			keymap = {
+				preset = "super-tab",
+				["<C-j>"] = { "select_next" },
+				["<C-k>"] = { "select_prev" },
+			},
+		},
+	},
+	{
+		"catgoose/nvim-colorizer.lua",
+		event = "BufReadPre",
+		opts = { -- set to setup table
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			sections = {
+				lualine_z = {},
+			},
+		},
+	},
 }
